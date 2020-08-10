9290b8b53b99e56d489f79747cebb6eb8df639c1
JaronArmiger <jaron.armiger@gmail.com>
2020-08-10 14:57:33 -0500

    add paypal_cancel and paypal_completed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get '/cancel', to: 'static_pages#paypal_cancel', as: 'paypal_cancel'
+  get '/completed', to: 'static_pages#paypal_completed', as: 'paypal_completed'
   resources :accounts, except: [:index, :destroy]
   resources :addresses, except: [:index, :show]
   resources :orders, except: [:edit, :update]
