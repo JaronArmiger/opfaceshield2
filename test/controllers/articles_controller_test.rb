e5681f754db46d2b2ea051be76cd02c7d270bd52
JaronArmiger <jaron.armiger@gmail.com>
2020-08-11 14:24:44 -0500

    test that all articles routes are unavailable to non-signed in user

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@first_user = users(:alfonso)
+  	@first_user.confirm
+  	@second_user = users(:ozuna)
+  	@second_user.confirm
+  	@admin = users(:admin)
+  	@admin.confirm
+  end
+
+  test "no routes should be accessible if no user signed in" do
+  	get articles_path
+  	assert_redirected_to new_user_session_path
+  	get new_article_path
+  	assert_redirected_to new_user_session_path
+  	article = articles(:one)
+  	get article_path(article)
+  	assert_redirected_to new_user_session_path
+  	get edit_article_path(article)
+  	assert_redirected_to new_user_session_path
+  	post articles_path
+  	assert_redirected_to new_user_session_path
+  	patch article_path(article)
+  	assert_redirected_to new_user_session_path
+  	delete article_path(article)
+  	assert_redirected_to new_user_session_path
+  end
+
+  test "non-admin can only access index, show" do
+  	
+  end
 end
---
+++ test/integration/articles_interface_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArticlesInterfaceTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
