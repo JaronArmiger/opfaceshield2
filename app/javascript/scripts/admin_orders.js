ea9e61b428e1b979723d4a1b5e17dde9b534f848
JaronArmiger <jaron.armiger@gmail.com>
2020-08-14 11:25:18 -0500

    modify admin_orders.js so that orders are only collapsed if screen width < 992px

--- app/javascript/scripts/admin_orders.js
+++ app/javascript/scripts/admin_orders.js
@@ -1,11 +1,12 @@
 $(document).on('turbolinks:load', function() {
-  const orderHeaderDivs = document.querySelectorAll('.admin-order-header-wrapper');
+  let x = window.matchMedia("(max-width: 992px)");
+  if (x.matches) {
+    const orderHeaderDivs = document.querySelectorAll('.admin-order-header-wrapper');
 
-  function toggleInfoDiv(e) {
-    this.nextElementSibling.classList.toggle('hide');
-  }
-
-  orderHeaderDivs.forEach(div => div.addEventListener('click', toggleInfoDiv))
-
-
+    function toggleInfoDiv(e) {
+      this.nextElementSibling.classList.toggle('hide');
+    }
+    orderHeaderDivs.forEach(div => div.nextElementSibling.classList.toggle('hide'));
+    orderHeaderDivs.forEach(div => div.addEventListener('click', toggleInfoDiv));
+  };
 });
