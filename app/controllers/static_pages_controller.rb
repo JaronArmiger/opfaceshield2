7d495f5da4b38f0cb94e23a771843794bb342013
JaronArmiger <jaron.armiger@gmail.com>
2020-08-24 17:27:00 -0500

    remove sign up form from home page bc heroku is being an ass

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   include StaticPagesHelper
   
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!
   def home
     @articles = Article.order(created_at: :desc).limit(3)
   end
